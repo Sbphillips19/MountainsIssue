43db0a2f77594003b44a9dfecc151637fcb7f3fa
Stephen Phillips <stephen@wellthon.com>
2019-11-11 21:13:49 -0700

    adding in main file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MountainsIssue
 github issue with firebase seed
+
+how i seed my database
+mountain info is the node I am updating
+`firebase database:set /MountainInfo mountains.js firebaseDB`
---
+++ mountains.js
@@ -0,0 +1,358 @@
+{
+    "ABasin": {
+      "latitude": 39.6425,
+        "longitude": -105.8719,
+          "name": "Arapahoe Basin"
+    },
+    "AlpineMeadows": {
+      "latitude": 39.1648,
+        "longitude": -120.2384,
+          "name": "Alpine Meadows"
+    },
+    "Alta": {
+      "latitude": 40.5888,
+        "longitude": -111.638,
+          "name": "Alta"
+    },
+    "Alyeska": {
+      "latitude": 64.2008,
+        "longitude": -149.4937,
+          "name": "Alyeska"
+    },
+    "Aspen": {
+      "latitude": 39.1911,
+        "longitude": -106.8175,
+          "name": "Aspen"
+    },
+    "Bachelor": {
+      "latitude": 43.9792,
+        "longitude": -121.6886,
+          "name": "Mount Bachelor"
+    },
+    "Baker": {
+      "latitude": 48.7767,
+        "longitude": -121.8144,
+          "name": "Mount Baker"
+    },
+    "BeaverCreek": {
+      "latitude": 39.6043,
+        "longitude": -106.5167,
+          "name": "Beaver Creek"
+    },
+    "BigSky": {
+      "latitude": 45.2618,
+        "longitude": -111.308,
+          "name": "Big Sky"
+    },
+    "BlueMountain": {
+      "latitude": 44.5405,
+        "longitude": -80.4081,
+          "name": "Blue Mountain"
+    },
+    "Boyne": {
+      "latitude": 45.2167,
+        "longitude": -85.0139,
+          "name": "Boyne Mountain"
+    },
+    "Breckenridge": {
+      "latitude": 39.4802,
+        "longitude": -106.0667,
+          "name": "Breckenridge"
+    },
+    "Buller": {
+      "latitude": -37.145,
+        "longitude": 146.4261,
+          "name": "Mount Buller"
+    },
+    "Cascade": {
+      "latitude": 43.5037,
+        "longitude": -89.5173,
+          "name": "Cascade Mountain"
+    },
+    "CoronetPeak": {
+      "latitude": -44.9272,
+        "longitude": 168.7363,
+          "name": "Coronet Peak"
+    },
+    "CrestedButte": {
+      "latitude": 38.899137,
+        "longitude": -106.965395,
+          "name": "Crested Butte"
+    },
+    "Copper": {
+      "latitude": 39.5021,
+        "longitude": -106.151,
+          "name": "Copper Mountain"
+    },
+    "Crystal": {
+      "latitude": 46.9282,
+        "longitude": -121.5045,
+          "name": "Crystal Mountain"
+    },
+    "Cypress": {
+      "latitude": 49.396,
+        "longitude": -123.2045,
+          "name": "Cypress Mountain"
+    },
+    "DeerValley": {
+      "latitude": 40.6374,
+        "longitude": -111.4783,
+          "name": "Deer Valley"
+    },
+    "Eldora": {
+      "latitude": 39.9142,
+        "longitude": -105.2947,
+          "name": "Eldora Mountain"
+    },
+    "Gore": {
+      "latitude": 43.6733,
+        "longitude": -74.0068,
+          "name": "Gore Mountain"
+    },
+    "GranitePeak": {
+      "latitude": 44.928611,
+        "longitude": -89.6825,
+          "name": "Granite Peak"
+    },
+    "Heavenly": {
+      "latitude": 38.9353,
+        "longitude": -119.94,
+          "name": "Heavenly"
+    },
+    "Hood": {
+      "latitude": 45.3736,
+        "longitude": -121.696,
+          "name": "Mount Hood"
+    },
+    "JacksonHole": {
+      "latitude": 43.4799,
+        "longitude": -110.7624,
+          "name": "Jackson Hole"
+    },
+    "JayPeak": {
+      "latitude": 44.9380,
+        "longitude": -72.5045,
+          "name": "Jay Peak"
+    },
+    "June": {
+      "latitude": 37.7679,
+        "longitude": -119.0906,
+          "name": "June Mountain"
+    },
+    "Keystone": {
+      "latitude": 39.6084,
+        "longitude": -105.9437,
+          "name": "Keystone"
+    },
+    "Killington": {
+      "latitude": 43.6045,
+        "longitude": -72.8201,
+          "name": "Killington Peak"
+    },
+    "Kirkwood": {
+      "latitude": 38.6848,
+        "longitude": -120.0652,
+          "name": "Kirkwood"
+    },
+    "LakeLouise": {
+      "latitude": 51.4254,
+        "longitude": -116.1773,
+          "name": "Lake Louise"
+    },
+    "Loon": {
+      "latitude": 44.036,
+        "longitude": -71.6214,
+          "name": "Loon Mountain"
+    },
+    "Loveland": {
+      "latitude": 39.6800,
+        "longitude": -105.8979,
+          "name": "Loveland"
+    },
+    "Mammoth": {
+      "latitude": 37.6308,
+        "longitude": -119.0326,
+          "name": "Mammoth Mountain"
+    },
+    "MtHutt": {
+      "latitude": -43.4717,
+        "longitude": 171.5264,
+          "name": "Mount Hutt"
+    },
+    "MtSnow": {
+      "latitude": 42.9602,
+        "Longitude": 72.9204,
+          "name": "Mount Snow"
+    },
+    "Northstar": {
+      "latitude": 39.2746,
+        "longitude": -120.1211,
+          "name": "Northstar"
+    },
+    "Norquay": {
+      "latitude": 51.2015,
+        "longitude": -115.5999,
+          "name": "Mount Norquay"
+    },
+    "Okemo": {
+      "latitude": 43.4018,
+        "longitude": 72.7166,
+          "name": "Okemo"
+    },
+    "ParkCity": {
+      "latitude": 40.6514,
+        "longitude": -111.508,
+          "name": "Park City"
+    },
+    "Purgatory": {
+      "latitude": 37.6303,
+        "longitude": -107.8140,
+          "name": "Purgatory"
+    },
+    "Revelstoke": {
+      "latitude": 50.9981,
+        "longitude": -118.1957,
+          "name": "Revelstoke"
+    },
+    "StevensPass": {
+      "latitude": 47.7448,
+        "longitude": -121.089,
+          "name": "Stevens Pass"
+    },
+    "SmugglersNotch": {
+      "latitude": 44.5885,
+        "longitude": 72.7900,
+          "name": "Smugglers' Notch"
+    },
+    "Snowbasin": {
+      "latitude": 41.216,
+        "longitude": -111.8569,
+          "name": "Snowbasin Resort"
+    },
+    "Snowbowl": {
+      "latitude": 40.581111,
+        "longitude": -111.7103,
+          "name": "Snowbowl"
+    },
+    "Snowbird": {
+      "latitude": 35.3309,
+        "longitude": -111.656389,
+          "name": "Snowbird"
+    },
+    "Snowshoe": {
+      "latitude": 38.4108,
+        "longitude": -79.9936,
+          "name": "Snowshoe Mountain"
+    },
+    "Snowqualmie": {
+      "latitude": 47.5287,
+        "longitude": -121.8254,
+          "name": "Snowqualmie"
+    },
+    "Solitude": {
+      "latitude": 40.6199,
+        "longitude": -111.5919,
+          "name": "Solitude Mountain"
+    },
+    "SquawValley": {
+      "latitude": 39.197,
+        "longitude": -120.2357,
+          "name": "Squaw Valley"
+    },
+    "Steamboat": {
+      "latitude": 40.485,
+        "longitude": -106.8317,
+          "name": "Steamboat"
+    },
+    "Stratton": {
+      "latitude": 43.0429,
+        "longitude": -72.9109,
+          "name": "Stratton"
+    },
+    "Sugarbush": {
+      "latitude": 44.1359,
+        "longitude": -72.8944,
+          "name": "Sugarbush"
+    },
+    "Sugarloaf": {
+      "latitude": 45.0314,
+        "longitude": 70.3131,
+          "name": "Sugarloaf"
+    },
+    "Sunshine": {
+      "latitude": 51.1152,
+        "longitude": -115.7633,
+          "name": "Banff Sunshine"
+    },
+    "Stowe": {
+      "latitude": 44.5291,
+        "longitude": -72.7773,
+          "name": "Stowe Mountain"
+    },
+    "Sugerloaf": {
+      "latitude": 45.0314,
+        "longitude": -70.3131,
+          "name": "Stevens Pass"
+    },
+    "SundayRiver": {
+      "latitude": 44.4734,
+        "longitude": -70.8569,
+          "name": "Sunday River"
+    },
+    "SunValley": {
+      "latitude": 43.6971,
+        "longitude": -114.3517,
+          "name": "Sun Valley"
+    },
+    "Taos": {
+      "latitude": 36.4072,
+        "longitude": -105.5731,
+          "name": "Taos"
+    },
+    "Telluride": {
+      "latitude": 37.9375,
+        "longitude": -107.8123,
+          "name": "Telluride"
+    },
+    "Timberline": {
+      "latitude": 45.3311,
+        "longitude": -121.711,
+          "name": "Timberline Lodge"
+    },
+    "Tremblant": {
+      "latitude": 46.1185,
+        "longitude": -74.5962,
+          "name": "Mount-Tremblant"
+    },
+    "Tyrol": {
+      "latitude": 43.0460,
+        "longitude": -89.7805,
+          "name": "Tyrol Basin"
+    },
+    "Vail": {
+      "latitude": 39.6061,
+        "longitude": -106.355,
+          "name": "Vail"
+    },
+    "Whistler": {
+      "latitude": 50.116322,
+        "longitude": -122.957359,
+          "name": "Whistler Blackcomb"
+    },
+    "Whiteface": {
+      "latitude": 44.3659,
+        "longitude": -73.9026,
+          "name": "Whiteface"
+    },
+    "Wilmont": {
+      "latitude": 42.4990,
+        "longitude": -88.1877,
+          "name": "Wilmont"
+    },
+    "WinterPark": {
+      "latitude": 39.8868,
+        "longitude": -105.7625,
+          "name": "Winter Park"
+    }
+  }
+  
